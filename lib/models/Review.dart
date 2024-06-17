7c396ae7989ce34e605a83b874118aaa0a6f88f8
gebienha <gabyslaptop69@gmail.com>
2024-06-17 14:42:07 +0800

    update review model with usage period

--- lib/models/Review.dart
+++ lib/models/Review.dart
@@ -8,18 +8,21 @@ class ReviewsProvider with ChangeNotifier {
       'comment': 'Great product! Highly recommend.',
       'image': 'assets/images/user-1.png',
       'name': 'Kim Seokjin',
+      'usagePeriod': '1 week',
     },
     {
       'rating': 4,
       'comment': 'Good quality, but a bit expensive.',
       'image': 'assets/images/user-2.jpeg',
       'name': 'Cha Eunwoo',
+      'usagePeriod': '1 day',
     },
     {
       'rating': 3,
       'comment': 'Average product, not bad but not great either.',
       'image': 'assets/images/user-3.jpg',
       'name': 'Jang Wonyoung',
+      'usagePeriod': '2 weeks',
     },
   ];
 
