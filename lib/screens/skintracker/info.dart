5575e7c57b81484af90d79820a897f2ba2127045
jasminedavina <98434861+jasminedavina@users.noreply.github.com>
2024-06-23 22:11:40 +0800

    Update info.dart

--- lib/screens/skintracker/info.dart
+++ lib/screens/skintracker/info.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:smooth_page_indicator/smooth_page_indicator.dart';  // Import the package
 
 class TutorialPage extends StatefulWidget {
   static const routeName = '/skin-tracker-tutorial-page';
@@ -33,91 +34,109 @@ class _TutorialPageState extends State<TutorialPage> {
     final screenWidth = MediaQuery.of(context).size.width;
 
     return Scaffold(
-      body: PageView(
-        controller: controller,
+      body: Column(
         children: [
-          Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Transform(
-                transform: Matrix4.translationValues(
-                    (0 - _currentPageValue) * 0.7 * screenWidth, 0, 0),
-                child: Icon(Icons.add, size: 100.0, color: Theme.of(context).colorScheme.onBackground),
-              ),
-              SizedBox(height: 50),
-              Transform(
-                transform: Matrix4.translationValues(
-                    (0 - _currentPageValue) * 1.5 * screenWidth, 0, 0),
-                child: Padding(
-                  padding: const EdgeInsets.symmetric(horizontal: 20.0),
-                  child: Text(
-                    "Press the add button at the upper right corner to track a new skin condition.",
-                    textAlign: TextAlign.center,
-                    style: TextStyle(fontWeight: FontWeight.bold, fontSize: 18, color: Theme.of(context).colorScheme.onBackground),
-                  ),
-                ),
-              ),
-              SizedBox(height: 50),
-              Transform(
-                transform: Matrix4.translationValues(
-                    (0 - _currentPageValue) * 2.5 * screenWidth, 0, 0),
-                child: Align(
-                  alignment: Alignment.center,
-                  child: Padding(
-                    padding: const EdgeInsets.symmetric(vertical: 10.0),
-                    child: IconButton(
-                      onPressed: () => controller.nextPage(
-                          duration: Duration(milliseconds: 300),
-                          curve: Curves.easeInOutExpo),
-                      icon: Icon(Icons.arrow_forward, size: 30),
-                      color: Colors.blue,
-                      padding: EdgeInsets.symmetric(horizontal: 20.0, vertical: 15.0),
+          Expanded(
+            child: PageView(
+              controller: controller,
+              children: [
+                Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Transform(
+                      transform: Matrix4.translationValues(
+                          (0 - _currentPageValue) * 0.5 * screenWidth, 0, 0), // Adjusted speed
+                      child: Icon(Icons.add, size: 100.0, color: const Color.fromARGB(255, 0, 0, 0)),
                     ),
-                  ),
-                ),
-              ),
-            ],
-          ),
-          Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Transform(
-                transform: Matrix4.translationValues(
-                    (1 - _currentPageValue) * 1.5 * screenWidth, 0, 0),
-                child: Icon(Icons.swipe, size: 100.0, color: Theme.of(context).colorScheme.onBackground),
-              ),
-              SizedBox(height: 50),
-              Transform(
-                transform: Matrix4.translationValues(
-                    (1 - _currentPageValue) * 1.5 * screenWidth, 0, 0),
-                child: Padding(
-                  padding: const EdgeInsets.symmetric(horizontal: 20.0),
-                  child: Text(
-                    "Swipe a tracked condition left or right to delete it.",
-                    textAlign: TextAlign.center,
-                    style: TextStyle(fontWeight: FontWeight.bold, fontSize: 18, color: Theme.of(context).colorScheme.onBackground),
-                  ),
-                ),
-              ),
-              SizedBox(height: 50),
-              Transform(
-                transform: Matrix4.translationValues(
-                    (1 - _currentPageValue) * 2.5 * screenWidth, 0, 0),
-                child: Align(
-                  alignment: Alignment.center,
-                  child: Padding(
-                    padding: const EdgeInsets.symmetric(vertical: 10.0),
-                    child: IconButton(
-                      onPressed: () => Navigator.of(context).pop(),
-                      icon: Icon(Icons.check, size: 30),
-                      color: Colors.blue,
-                      padding: EdgeInsets.symmetric(horizontal: 20.0, vertical: 15.0),
+                    SizedBox(height: 50),
+                    Transform(
+                      transform: Matrix4.translationValues(
+                          (0 - _currentPageValue) * 1.2 * screenWidth, 0, 0), // Adjusted speed
+                      child: Padding(
+                        padding: const EdgeInsets.symmetric(horizontal: 20.0),
+                        child: Text(
+                          "Press the add button at the upper right corner to track a new skin condition.",
+                          textAlign: TextAlign.center,
+                          style: TextStyle(fontWeight: FontWeight.bold, fontSize: 18),
+                        ),
+                      ),
                     ),
-                  ),
+                    SizedBox(height: 50),
+                    Transform(
+                      transform: Matrix4.translationValues(
+                          (0 - _currentPageValue) * 2.0 * screenWidth, 0, 0), // Adjusted speed
+                      child: Align(
+                        alignment: Alignment.center,
+                        child: Padding(
+                          padding: const EdgeInsets.symmetric(vertical: 10.0),
+                          child: IconButton(
+                            onPressed: () => controller.nextPage(
+                                duration: Duration(milliseconds: 600),
+                                curve: Curves.easeInOutExpo),
+                            icon: Icon(Icons.arrow_forward, size: 30),
+                            color: Colors.blue,
+                            padding: EdgeInsets.symmetric(horizontal: 20.0, vertical: 15.0),
+                          ),
+                        ),
+                      ),
+                    ),
+                  ],
                 ),
-              ),
-            ],
+                Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Transform(
+                      transform: Matrix4.translationValues(
+                          (1 - _currentPageValue) * 0.5 * screenWidth, 0, 0), // Adjusted speed
+                      child: Icon(Icons.swipe, size: 100.0, color: const Color.fromARGB(255, 0, 0, 0)),
+                    ),
+                    SizedBox(height: 50),
+                    Transform(
+                      transform: Matrix4.translationValues(
+                          (1 - _currentPageValue) * 1.2 * screenWidth, 0, 0), // Adjusted speed
+                      child: Padding(
+                        padding: const EdgeInsets.symmetric(horizontal: 20.0),
+                        child: Text(
+                          "Swipe a tracked condition left or right to delete it.",
+                          textAlign: TextAlign.center,
+                          style: TextStyle(fontWeight: FontWeight.bold, fontSize: 18),
+                        ),
+                      ),
+                    ),
+                    SizedBox(height: 50),
+                    Transform(
+                      transform: Matrix4.translationValues(
+                          (1 - _currentPageValue) * 2.0 * screenWidth, 0, 0), // Adjusted speed
+                      child: Align(
+                        alignment: Alignment.center,
+                        child: Padding(
+                          padding: const EdgeInsets.symmetric(vertical: 10.0),
+                          child: IconButton(
+                            onPressed: () => Navigator.of(context).pop(),
+                            icon: Icon(Icons.check, size: 30),
+                            color: Colors.blue,
+                            padding: EdgeInsets.symmetric(horizontal: 20.0, vertical: 15.0),
+                          ),
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ],
+            ),
           ),
+          SizedBox(height: 20),
+          SmoothPageIndicator(
+            controller: controller,  // PageController
+            count: 2,
+            effect: WormEffect(
+              dotHeight: 12,
+              dotWidth: 12,
+              activeDotColor: Colors.blue,
+              dotColor: Colors.grey,
+            ),
+          ),
+          SizedBox(height: 20),
         ],
       ),
     );
