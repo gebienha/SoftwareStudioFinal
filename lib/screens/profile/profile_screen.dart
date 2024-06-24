9ddebb08a02b2466859174077f2a1e966d1d9e6c
anjitije <144112291+anjitije@users.noreply.github.com>
2024-06-25 01:01:50 +0800

    Update profile_screen.dart

--- lib/screens/profile/profile_screen.dart
+++ lib/screens/profile/profile_screen.dart
@@ -1,15 +1,40 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
-import 'package:shop_app/screens/profile/components/add_avatar_page.dart';
-import 'package:shop_app/screens/profile/components/my_profile.dart';
 import 'package:shop_app/screens/sign_in/sign_in_screen.dart';
 import 'components/profile_menu.dart';
+import 'components/firestore.dart';
 import 'components/profile_pic.dart';
 import '../../screens/skintracker/skin_tracker.dart';
-import 'package:shop_app/screens/init_screen.dart';
 
-class ProfileScreen extends StatelessWidget {
+class ProfileScreen extends StatefulWidget {
   static String routeName = "/profile";
+
+  const ProfileScreen({super.key});
+
+  @override
+  _ProfileScreenState createState() => _ProfileScreenState();
+}
+
+class _ProfileScreenState extends State<ProfileScreen> {
+  String? avatarSvgData;
+  String? displayName;
+  bool isLoading = true;
+
+  @override
+  void initState() {
+    super.initState();
+    _fetchUserProfile();
+  }
+
+  void _fetchUserProfile() async {
+    UserProfileService userProfileService = UserProfileService();
+    Map<String, String> profileData = await userProfileService.fetchUserProfile();
+    setState(() {
+      avatarSvgData = profileData['avatarSvg'];
+      displayName = profileData['name'];
+      isLoading = false;
+    });
+  }
 
   void signUserOut(BuildContext context) {
     FirebaseAuth.instance.signOut().then((_) {
@@ -21,8 +46,6 @@ class ProfileScreen extends StatelessWidget {
     });
   }
 
-  const ProfileScreen({super.key});
-  
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -33,47 +56,50 @@ class ProfileScreen extends StatelessWidget {
         iconTheme: const IconThemeData(color: Color(0xFF60C6A2)),
         leading: const SizedBox(), // This removes the back arrow
       ),
-      body: SingleChildScrollView(
-        padding: const EdgeInsets.symmetric(vertical: 20),
-        child: Column(
-          children: [
-            const ProfilePic(),
-            const SizedBox(height: 20),
-            ProfileMenu(
-              text: "My Profile",
-              icon: "assets/icons/User Icon.svg",
-              press: () {
-                Navigator.pushNamed(context, MyProfilePage.routeName);
-              },
+      body: isLoading
+          ? Center(child: CircularProgressIndicator())
+          : SingleChildScrollView(
+              padding: const EdgeInsets.symmetric(vertical: 20),
+              child: Column(
+                children: [
+                  ProfilePic(
+                    initialAvatarSvgData: avatarSvgData,
+                    initialName: displayName,
+                  ),
+                  const SizedBox(height: 20),
+                  // ProfileMenu(
+                  //   text: "My Account",
+                  //   icon: "assets/icons/User Icon.svg",
+                  //   press: () => {},
+                  // ),
+                  // ProfileMenu(
+                  //   text: "Notifications",
+                  //   icon: "assets/icons/Bell.svg",
+                  //   press: () {},
+                  // ),
+                  // ProfileMenu(
+                  //   text: "Settings",
+                  //   icon: "assets/icons/Settings.svg",
+                  //   press: () {},
+                  // ),
+                  ProfileMenu(
+                    text: "Skin Tracker",
+                    icon: "assets/icons/Question mark.svg",
+                    press: () {
+                      Navigator.pushNamed(
+                        context,
+                        SkinTracker.routeName,
+                      );
+                    },
+                  ),
+                  ProfileMenu(
+                    text: "Log Out",
+                    icon: "assets/icons/Log out.svg",
+                    press: () => signUserOut(context),
+                  ),
+                ],
+              ),
             ),
-            // ProfileMenu(
-            //   text: "Notifications",
-            //   icon: "assets/icons/Bell.svg",
-            //   press: () {},
-            // ),
-            // ProfileMenu(
-            //   text: "Settings",
-            //   icon: "assets/icons/Settings.svg",
-            //   press: () {},
-            // ),
-            ProfileMenu(
-              text: "Skin Tracker",
-              icon: "assets/icons/calendar.svg",
-              press: () {
-                Navigator.pushNamed(
-                  context,
-                  SkinTracker.routeName,
-                );
-              },
-            ),
-            ProfileMenu(
-              text: "Log Out",
-              icon: "assets/icons/Log out.svg",
-              press: () => signUserOut(context),
-            ),
-          ],
-        ),
-      ),
     );
   }
 }
